4cc128833b0b147d7d78db3b6e899126c1a5e7c7
“Joao <joaonada@icloud.com>
2026-05-14 19:07:55 -0400

    commit project 14

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -26,7 +26,7 @@ module.exports = (req, res, next) => {
     return handleAuthError(res);
   }
 
-  req.user = payload; // adding the payload to the Request object
+  req.user = payload;
 
-  return next(); // passing the request further along
+  return next(); 
 };
