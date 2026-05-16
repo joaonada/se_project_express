f7e55075481a02d6a53353c541a033042be1ff67
“Joao <joaonada@icloud.com>
2026-05-16 14:31:13 -0400

    new commit project 14

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const { login, createUser } = require('./controllers/users');
 
 const { PORT = 3001 } = process.env;
 const app = express();
@@ -11,13 +12,17 @@ const indexRouter = require("./routes/index");
 app.use(cors());
 const mongoUri = process.env.MONGODB_URI;
 module.exports.jwtSecret = process.env.JWT_SECRET;
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db")
+mongoose.connect(mongoUri || "mongodb://127.0.0.1:27017/wtwr_db")
+// eslint-disable-next-line no-console
 .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('MongoDB connection error:', error));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.post('/signin', login);
+app.post('/signup', createUser);
 
 app.use("/", indexRouter);
 
